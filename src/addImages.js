c8b16215936e056ac3b5e504ec08387db74d2383
trpgd <65950880+wiklyhi@users.noreply.github.com>
2023-09-11 02:18:46 +0500

    Update addImages.js

--- src/addImages.js
+++ src/addImages.js
@@ -1,43 +1,59 @@
-export { add12img, add24img, add18img, add36img }
-import { addDivImages } from "./index.js";
-import { imgArray } from "./imgArray.js"
+Экспорт { add12img, add24img, add18img, add36img }
+Импортировать { addDivImages } from "./index.js";
+Импортировать { imgArray } from "./imgArray.js";
 
-function showImages(img) {
-  let a = Math.floor(Math.random() * imgArray.length);
-  img = imgArray[a] // рандомный объект с атрибутами
-  return img
-}
+let cardNum = 0;
+function addImages() {
+  let randomId = []
 
-function AddImages() { //add div images
-  let content = showImages()
-  addDivImages.innerHTML +=
+  for (var t = 0; t < cardNum; t++) {
+    rand()
+  }
+
+  function rand() {
+    let ran = imgArray[Math.floor(Math.random() * imgArray.length)]
+    if (randomId.indexOf(ran) === -1) {
+      randomId.push(ran)
+    } else {
+      rand()
+    }
+  }
+
+  const toHTML = random =>
     `<div class="mui--text-center mui-col-xs-6 mui-col-md-2">
       <div class="mui-panel">
-        <img class="non-class" src="${content.img}" alt="${content.title}">
+        <img class="non-class" src="${random.img}" alt="${random.title}">
       </div>
     </div>`;
+
+
+  function render() {
+    const html = randomId.map(random => toHTML(random)).join('')
+    addDivImages.innerHTML = html
+  }
+
+  render()
+
 }
 
 function add12img() {
-  for (let i = 0; i < 12; i++) {
-    AddImages();
-  }
+  cardNum = 12
+  addImages();
 }
 
 function add18img() {
-  for (let i = 0; i < 18; i++) {
-    AddImages();
-  }
+  cardNum = 18
+  addImages();
+
 }
 
 function add24img() {
-  for (let i = 0; i < 24; i++) {
-    AddImages();
-  }
+  cardNum = 24
+  addImages();
 }
 
+
 function add36img() {
-  for (let i = 0; i < 36; i++) {
-    AddImages();
-  }
+  cardNum = 36
+  addImages();
 }
